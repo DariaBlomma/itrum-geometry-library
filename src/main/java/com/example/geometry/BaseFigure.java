c033c2f3168c6b1f4fe257055a47dcdd69f2dc04
Daria Kozina <kozina.flowers@yandex.ru>
2026-04-06 12:01:47 +0300

    added GetColor method

--- src/main/java/com/example/geometry/BaseFigure.java
+++ src/main/java/com/example/geometry/BaseFigure.java
@@ -1,6 +1,9 @@
 package com.example.geometry;
 
 public interface BaseFigure {
+    default String getColor() {
+        return "purple";
+    }
     double getArea();
     double getPerimeter();
 }
