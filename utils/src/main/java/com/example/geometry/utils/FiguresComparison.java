3c5dd12a939642b552edd972e81a7c66b6c7e9ed
Daria Kozina <kozina.flowers@yandex.ru>
2026-04-06 14:46:16 +0300

    some changes

--- utils/src/main/java/com/example/geometry/utils/FiguresComparison.java
+++ utils/src/main/java/com/example/geometry/utils/FiguresComparison.java
@@ -3,12 +3,12 @@ package com.example.geometry.utils;
 import com.example.geometry.BaseFigure;
 import com.example.geometry.BaseTriangle;
 
-public class Comparison {
+public class FiguresComparison {
     /**
      * Сравнивает две фигуры по площади.
      * @return 1, -1 или 0
      */
-    int compareByArea(BaseFigure a, BaseTriangle b) {
+     public static int  compareByArea(BaseFigure a, BaseTriangle b) {
         double area1 = a.getArea();
         double area2 = b.getArea();
 
