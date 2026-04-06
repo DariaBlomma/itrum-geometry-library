7025d7781337879ee4374bba3cfe2af174f08cc8
Daria Kozina <kozina.flowers@yandex.ru>
2026-04-06 14:56:39 +0300

    added small feature to utils

--- utils/src/main/java/com/example/geometry/utils/FiguresComparison.java
+++ utils/src/main/java/com/example/geometry/utils/FiguresComparison.java
@@ -1,16 +1,27 @@
 package com.example.geometry.utils;
 
 import com.example.geometry.BaseFigure;
-import com.example.geometry.BaseTriangle;
 
 public class FiguresComparison {
     /**
-     * Сравнивает две фигуры по площади.
+     * Compares two figures by area
      * @return 1, -1 или 0
      */
      public static int compareByArea(BaseFigure a, BaseFigure b) {
         double area1 = a.getArea();
         double area2 = b.getArea();
+
+        return Double.compare(area1, area2);
+    }
+
+    /**
+     * Compares two figures by perimeter
+     * @return 1, -1 или 0
+     */
+
+    public static int compareByPerimeter(BaseFigure a, BaseFigure b) {
+        double area1 = a.getPerimeter();
+        double area2 = b.getPerimeter();
 
         return Double.compare(area1, area2);
     }
